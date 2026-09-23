0cae56b424fe1e806cbb00f2077871a7f1cca028
Zhai Jinpeng <914962409@qq.com>
2026-09-23 22:53:01 +0800

    Fix: gitignore

--- src/run_gui.py
+++ src/run_gui.py
@@ -3,6 +3,7 @@ This script is required to help PyInstaller to locate the exterior_ballistics pa
 And pretty useful for debugging.
 """
 
+
 from exterior_ballistics.gui import main
 from multiprocessing import freeze_support
 
